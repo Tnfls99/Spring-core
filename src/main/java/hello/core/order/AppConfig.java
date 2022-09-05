8d3b90ac862ef426d902a0b22166af3e14849b54
Surin Lee <leesurin99@gmail.com>
2022-09-05 13:08:55 +0900

    (refactor)AppConfig 리팩토링

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -1,17 +1,31 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
-    }
+        return new MemberServiceImpl(memberRepository());
+    } // 역할
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    } // 구현
+
+    // 위와 같이 분리하여 작성하면 역할이 뚜렷하게 드러남.
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
